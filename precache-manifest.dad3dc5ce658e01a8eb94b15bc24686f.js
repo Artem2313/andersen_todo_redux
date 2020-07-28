bc066ca2126236e510f2d29e6b00b8b6735f1851
Artem2313 <huselnykov@gmail.com>
2020-07-28 23:15:15 +0300

    Updates

---
+++ precache-manifest.dad3dc5ce658e01a8eb94b15bc24686f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275d7f048bdfe7b1d9f8e567196308ad",
+    "url": "/andersen_todo_redux/index.html"
+  },
+  {
+    "revision": "82835693126ddb3b6349",
+    "url": "/andersen_todo_redux/static/css/main.0e90cb8d.chunk.css"
+  },
+  {
+    "revision": "8f4d686383443e9ef94c",
+    "url": "/andersen_todo_redux/static/js/2.121a251c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/andersen_todo_redux/static/js/2.121a251c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82835693126ddb3b6349",
+    "url": "/andersen_todo_redux/static/js/main.6d47a2c5.chunk.js"
+  },
+  {
+    "revision": "f39c09b59380bda23866",
+    "url": "/andersen_todo_redux/static/js/runtime-main.7ee794fb.js"
+  }
+]);
